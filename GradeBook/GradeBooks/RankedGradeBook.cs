86ab9c93ebf107c3d699be5d5a6b51546a81bde3
Kenneth Mitton <kmitton@webstaurantstore.com>
2018-11-10 12:11:21 -0500

    Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,37 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            var percentile = Students.Where(x => x.AverageGrade <= averageGrade).Count() / System.Convert.ToDouble(Students.Count);
+
+            if (percentile >= 0.8)
+            {
+                return 'A';
+            }
+            else if (percentile >= 0.6)
+            {
+                return 'B';
+            }
+            else if (percentile >= 0.4)
+            {
+                return 'C';
+            }
+            else if (percentile >= 0.2)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }

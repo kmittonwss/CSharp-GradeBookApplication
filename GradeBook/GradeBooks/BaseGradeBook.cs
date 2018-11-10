789d7a7b5d91f0c2559a5317f9d0ab1e0e5a8e97
Kenneth Mitton <kmitton@webstaurantstore.com>
2018-11-10 12:37:00 -0500

    Module 7

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,10 +14,12 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
@@ -107,18 +109,19 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var weightedModifier = (IsWeighted && studentType != StudentType.Standard) ? 1 : 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + weightedModifier;
                 case 'B':
-                    return 3;
+                    return 3 + weightedModifier;
                 case 'C':
-                    return 2;
+                    return 2 + weightedModifier;
                 case 'D':
-                    return 1;
+                    return 1 + weightedModifier;
                 case 'F':
-                    return 0;
+                    return 0 + weightedModifier;
             }
             return 0;
         }
--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,9 +4,9 @@ using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
             Type = GradeBookType.Ranked;
         }
